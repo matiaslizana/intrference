91cb6d2c779486e5eea72fd1ad358d7cb232ba8d
Matias Lizana <matias.lizana.garcia@gmail.com>
2020-05-20 16:49:17 +0200

    Lose samples added

--- intrference.cpp
+++ intrference.cpp
@@ -15,7 +15,7 @@ extern "C" {
 	F_EXPORT FMOD_DSP_DESCRIPTION* F_CALL FMODGetDSPDescription();
 }
 
-#define INTRF_NUM_PARAMETERS 3
+#define INTRF_NUM_PARAMETERS 4
 
 FMOD_RESULT F_CALLBACK IntrfReadCallback(FMOD_DSP_STATE *dsp_state, float *inbuffer, float *outbuffer, unsigned int length, int inchannels, int *outchannels);
 FMOD_RESULT F_CALLBACK IntrfCreateCallback(FMOD_DSP_STATE *dsp_state);
@@ -27,12 +27,14 @@ FMOD_RESULT F_CALLBACK IntrfGetParamFloatCallback(FMOD_DSP_STATE *dsp_state, int
 static FMOD_DSP_PARAMETER_DESC voice_shatter_desc;
 static FMOD_DSP_PARAMETER_DESC noise_volume_desc;
 static FMOD_DSP_PARAMETER_DESC noise_shatter_desc;
+static FMOD_DSP_PARAMETER_DESC lose_samples_desc;
 
 FMOD_DSP_PARAMETER_DESC *paramdesc[INTRF_NUM_PARAMETERS] =
 {
 	&voice_shatter_desc,
 	&noise_volume_desc,
-	&noise_shatter_desc
+	&noise_shatter_desc,
+	&lose_samples_desc
 };
 
 FMOD_DSP_DESCRIPTION FMOD_Intrference_Desc =
@@ -73,6 +75,7 @@ extern "C"
 		FMOD_DSP_INIT_PARAMDESC_FLOAT(voice_shatter_desc, "Voice Shatter", "%", "voice shatter in percent", 0, 100, 0);
 		FMOD_DSP_INIT_PARAMDESC_FLOAT(noise_volume_desc, "Noise Volume", "%", "noise volume in percent", 0, 100, 0);
 		FMOD_DSP_INIT_PARAMDESC_FLOAT(noise_shatter_desc, "Noise Shatter", "%", "noise shatter in percent", 0, 100, 0);
+		FMOD_DSP_INIT_PARAMDESC_FLOAT(lose_samples_desc, "Lose Samples", "%", "percentage of losing samples", 0, 100, 0);
 		return &FMOD_Intrference_Desc;
 	}
 
@@ -86,6 +89,7 @@ typedef struct
 	float voice_shatter;
 	float noise_volume;
 	float noise_shatter;
+	float lose_samples;
 
 	//Struct params
 	int   length_samples;
@@ -97,19 +101,28 @@ FMOD_RESULT F_CALLBACK IntrfReadCallback(FMOD_DSP_STATE *dsp_state, float *inbuf
 
 	intrf_data *data = (intrf_data *)dsp_state->plugindata;
 
-	//Volume that voice will have on each "window"
 	float data_vs = (float)(data->voice_shatter / 100);
 	float data_ns = (float)(data->noise_shatter / 100);
 	float data_nv = (float)(data->noise_volume / 100);
+	float data_ls = (float)(data->lose_samples / 100);
+
 	float voice_shatter = 1 - (((float)(rand() % 32768) / 16384.0f) - 1.0f) * data_vs;
 	float noise_shatter = 1 - (((float)(rand() % 32768) / 16384.0f) - 1.0f) * data_ns;
+
+	unsigned int sample_losed = data_ls == 1 ?  0 : (int) (1 / (1 - data_ls));
 
 	for (unsigned int samp = 0; samp < length; samp++) 
     { 
         for (int chan = 0; chan < *outchannels; chan++)
         {
+			//Checks if we have to lose the sample
+			bool add_voice = sample_losed == 0 ? false : samp % sample_losed == 0;
+			//Calculates noise
 			float noise = (((float)(rand() % 32768) / 16384.0f) - 1.0f) * data_nv * 0.1f * noise_shatter;
-			outbuffer[(samp * inchannels) + chan] = inbuffer[(samp * inchannels) + chan] * voice_shatter + noise;
+			//Calculates voice
+			float voice = add_voice ? inbuffer[(samp * inchannels) + chan] * voice_shatter : 0;
+			//Outbuffer
+			outbuffer[(samp * inchannels) + chan] = voice + noise;
 			data->buffer[(samp * *outchannels) + chan] = outbuffer[(samp * inchannels) + chan];
         }
     }
@@ -123,7 +136,6 @@ FMOD_RESULT F_CALLBACK IntrfCreateCallback(FMOD_DSP_STATE *dsp_state)
     FMOD_RESULT result;
 
     result = dsp_state->functions->getblocksize(dsp_state, &blocksize);
-    //ERRCHECK(result);
 
     intrf_data *data = (intrf_data *)calloc(sizeof(intrf_data), 1);
     if (!data)
@@ -134,6 +146,7 @@ FMOD_RESULT F_CALLBACK IntrfCreateCallback(FMOD_DSP_STATE *dsp_state)
 	data->voice_shatter = 0.0f;
 	data->noise_volume = 0.0f;
 	data->noise_shatter = 0.0f;
+	data->lose_samples = 0.0f;
     data->length_samples = blocksize;
 
     data->buffer = (float *)malloc(blocksize * 8 * sizeof(float));      // *8 = maximum size allowing room for 7.1.   Could ask dsp_state->functions->getspeakermode for the right speakermode to get real speaker count.
@@ -152,10 +165,9 @@ FMOD_RESULT F_CALLBACK IntrfReleaseCallback(FMOD_DSP_STATE *dsp_state)
         intrf_data *data = (intrf_data *)dsp_state->plugindata;
 
         if (data->buffer)
-        {
 			free(data->buffer);
-        }
-        free(data);
+       
+		free(data);
     }
 
     return FMOD_OK;
@@ -166,6 +178,7 @@ FMOD_RESULT F_CALLBACK IntrfResetCallback(FMOD_DSP_STATE *dsp_state) {
 	data->voice_shatter = 0.0f;
 	data->noise_volume = 0.0f;
 	data->noise_shatter = 0.0f;
+	data->lose_samples = 0.0f;
 	return FMOD_OK;
 }
 
@@ -178,6 +191,8 @@ FMOD_RESULT F_CALLBACK IntrfSetParamFloatCallback(FMOD_DSP_STATE *dsp_state, int
 		mydata->noise_volume = value;
 	else if (index == 2)
 		mydata->noise_shatter = value;
+	else if (index == 3)
+		mydata->lose_samples = value;
 	else
 		return FMOD_ERR_INVALID_PARAM;
 	return FMOD_OK;
@@ -193,6 +208,8 @@ FMOD_RESULT F_CALLBACK IntrfGetParamFloatCallback(FMOD_DSP_STATE *dsp_state, int
 		*value = mydata->noise_volume;
 	else if (index == 2)
 		*value = mydata->noise_shatter;
+	else if (index == 3)
+		*value = mydata->lose_samples;
 	else
 		return FMOD_ERR_INVALID_PARAM;
 	
